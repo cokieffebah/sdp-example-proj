8d303fe183dc3645e0296a3746302df4e0218e1a
Carlos OKieffe <okieffe_carlos@bah.com>
2018-12-11 11:19:38 -0500

    changed pipeline_config.groovy organization

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,1 +1,8 @@
 organization = "cokieffebah"
+application_image_repository = "docker-registry.default.svc:5000/demo"
+application_image_repository_credential = "docker-registry"
+
+libraries{
+  github_enterprise
+  docker
+}
